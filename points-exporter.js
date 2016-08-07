27904aaa9402faacc741fa8bad7c905300e1c1af
yangchangkun <1291364120@qq.com>
2016-08-07 20:30:36 +0800

    move four step

---
+++ points-exporter.js
@@ -0,0 +1,47 @@
+/**
+ * Created by ckyang on 16/8/7.
+ */
+var pointExport = ( function () {
+    return {
+        exportPoints: function () {
+            return {
+                point1: {left: 78, top: 320},
+                point4: {left: 395, top: 320},
+                point5: {left: 506, top: 320},
+                point7: {left: 506, top: 470},
+                "point8-l": {left: 446, top: 470},
+                "point8-h": {left: 446, top: 448},
+                "point9-h": {left: 343, top: 448},
+                "point9-l": {left: 343, top: 470},
+                point9: {left: 295, top: 470},
+                "point10-l": {left: 140, top: 475},
+                "point10-h": {left: 140, top: 448},
+                point11: {left: 75, top: 448},
+                point12: {left: 75, top: 520},
+                point13: {left: 75, top: 595},
+                "point13-l": {left: 130, top: 595},
+                "point13-h": {left: 130, top: 575},
+                "point14-h": {left: 223, top: 575},
+                "point14-l": {left: 223, top: 585},
+                point15: {left: 285, top: 585},
+                point17: {left: 505, top: 585},
+                point19: {left: 505, top: 730},
+                "point20-l": {left: 398, top: 730},
+                "point20-h": {left: 398, top: 710},
+                "point21-h": {left: 228, top: 710},
+                "point21-l": {left: 228, top: 730},
+                point22: {left: 195, top: 730},
+                point23: {left: 75, top: 730},
+                point25: {left: 75, top: 878},
+                "point25-l": {left: 135, top: 878},
+                "point25-h": {left: 135, top: 850},
+                "point26-h": {left: 240, top: 850},
+                "point26-l": {left: 240, top: 872},
+                point28: {left: 396, top: 872},
+                point29: {left: 505, top: 872},
+                point30: {left: 490, top: 990}
+            }
+        }
+        
+    }
+})();
